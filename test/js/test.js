da3889870a6b812cf94d8cce4d3b78d70c851e03
zhangjie1 <zhangjie1@hujiang.com>
2015-08-12 10:30:16 +0800

    update

--- test/js/test.js
+++ test/js/test.js
@@ -1,7 +1,13 @@
 ﻿//插件
 ;
 (function ($) {
-	$.sum = function () {
-
+	$.sum = function (array) {
+		var total = 0;
+		$.each(array, function (index, value) {
+			value = $.trim(value);
+			value = parseFloat(value) || 0;
+			total = total + value;
+		});
+		return total;
 	};
 })(jQuery);
