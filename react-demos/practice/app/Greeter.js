b42f544ee2ce79344c97a9fdf6ee4293ac326c62
张捷Oral <zhangjie1@hujiang.com>
2017-02-04 16:37:06 +0800

    add: practice 打包实战

---
+++ react-demos/practice/app/Greeter.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+import config from './config.json'
+import styles from './Greeter.styl'
+import classNames from 'classnames/bind'
+
+let cx = classNames.bind(styles)
+
+class Greeter extends Component{
+	render() {
+		return (
+			<div className={cx('root-root')}>
+				<p className={'max'}>name</p>
+				{config.greetText}
+			</div>
+		)
+	}
+}
+export default Greeter
---
+++ react-demos/practice/webpack.config.js
@@ -0,0 +1,44 @@
+﻿// “__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录。
+module.exports = {
+	devtool: 'eval-source-map',
+	entry: __dirname + '/app/main.js',// 入口文件
+	output: {
+		path: __dirname + '/public',// 打包后的文件存放的地方
+		filename: 'bundle.js'// 打包后输出文件的文件名
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.json$/,
+				loader: 'json'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			},
+			{
+				test: /\.css$/,
+				loader: 'style!css?modules!postcss'
+			},
+			{
+				test: /\.styl$/,
+			  loader: 'style-loader!css-loader?modules&localIdentName=[path][name]-[local]-[hash:base64:5]!stylus-loader'
+			}
+		]
+	},
+
+	postcss: [
+		require('autoprefixer')
+	],
+	devServer: {
+		contentBase: './public',// 默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在的目录
+		port: 8080,// 监听端口
+		colors: true,// 使终端输出的文件是彩色的
+		historyApiFallback: true,// 依赖于HTML5 history API，如果设置为true,所有的跳转将指向index.html,单页应用有用
+		inline: true// 当源文件改变时会自动刷新页面
+	}
+}
