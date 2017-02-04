e3ebb6df0c0ab565ae6c6d33962dd6b31a3a0440
张捷Oral <zhangjie1@hujiang.com>
2017-02-04 18:22:27 +0800

    add: 增加webpack中插件HtmlWebpackPlugin使用

--- react-demos/practice/webpack.config.js
+++ react-demos/practice/webpack.config.js
@@ -1,9 +1,12 @@
 ﻿// “__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录。
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
 	devtool: 'eval-source-map',
 	entry: __dirname + '/app/main.js',// 入口文件
 	output: {
-		path: __dirname + '/public',// 打包后的文件存放的地方
+		path: __dirname + '/build',// 打包后的文件存放的地方
 		filename: 'bundle.js'// 打包后输出文件的文件名
 	},
 	module: {
@@ -34,8 +37,15 @@ module.exports = {
 	postcss: [
 		require('autoprefixer')
 	],
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: __dirname + "/app/index.tmpl.html"
+		})
+	],
+
 	devServer: {
-		contentBase: './public',// 默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在的目录
+		contentBase: './build',// 默认webpack-dev-server会为根文件夹提供本地服务器，如果想为另外一个目录下的文件提供本地服务器，应该在这里设置其所在的目录
 		port: 8080,// 监听端口
 		colors: true,// 使终端输出的文件是彩色的
 		historyApiFallback: true,// 依赖于HTML5 history API，如果设置为true,所有的跳转将指向index.html,单页应用有用
