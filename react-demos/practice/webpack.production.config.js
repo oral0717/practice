a39bada423245aa77561df2fd8e6e6d0316a4d66
张捷Oral <zhangjie1@hujiang.com>
2017-02-06 10:32:27 +0800

    add: 增加生产环境中的webpack配置文件

--- react-demos/practice/webpack.production.config.js
+++ react-demos/practice/webpack.production.config.js
@@ -1,13 +1,12 @@
-﻿// “__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录。
+// “__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录。
 var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	devtool: 'eval-source-map',
 	entry: __dirname + '/app/main.js',// 入口文件
 	output: {
 		path: __dirname + '/build',// 打包后的文件存放的地方
-		filename: '[name]-[hash].js'// 打包后输出文件的文件名
+		filename: 'bundle.js'// 打包后输出文件的文件名
 	},
 	module: {
 		loaders: [
