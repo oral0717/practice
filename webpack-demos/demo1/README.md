acb5324d195eb778e77a5a9926be2bdfc6bdc96e
张捷Oral <zhangjie1@hujiang.com>
2019-03-20 20:19:08 +0800

    no message

--- webpack-demos/demo1/README.md
+++
@@ -1,2 +0,0 @@
-# webpack-demos
-http://localhost:8080
--- webpack-demos/demo1/app/main.js
+++
@@ -1,8 +0,0 @@
-﻿import React from 'react'
-import { render } from 'react-dom'
-import Page from './page'
-
-render(
-	<Page />,
-	document.getElementById('root')
-)
