826de133792d07b49e87ce9dd65c6c1d20667fdc
oral0717 <807910824@qq.com>
2019-07-02 00:40:12 +0800

    update

---
+++ exercise/seen/js/slide.js
@@ -0,0 +1,65 @@
+var banner = document.querySelector(".banner");
+var rightBtn = document.querySelector(".lr-btn .right");
+var leftBtn = document.querySelector(".lr-btn .left");
+var images = document.querySelectorAll(".img img");
+var tabsParent = document.querySelector(".btn-list");
+var tabs = document.querySelectorAll(".btn-list li")
+var index = 0;
+
+// 定时器
+var timer = null;
+
+window.onload = function(){
+	timer = setInterval("change(true)", 3000);
+};
+
+rightBtn.onclick = function(){
+	clearInterval(timer);
+	change(true);
+	timer = setInterval("change(true)", 3000);
+}
+
+leftBtn.onclick = function(){
+	clearInterval(timer);
+	change(false);
+	timer = setInterval("change(true)", 3000);
+}
+
+tabsParent.onmouseover = function(event){
+	var curTab = event.target;
+	var curTabTag = curTab.tagName.toLowerCase();
+	if (curTabTag !== "li") {return false;}
+	clearInterval(timer);
+	index = parseInt(curTab.dataset.index);
+	if(index >= 0){
+		for(var i=0;i<images.length;i++){
+			images[i].className = "";
+			tabs[i].className = "";
+		}
+		images[index].className = "on";
+		tabs[index].className = "on";
+	}
+
+}
+
+tabsParent.onmouseout = function(event) {
+	var curTab = event.target;
+	var curTabTag = curTab.tagName.toLowerCase();
+	if (curTabTag !== "li") {return false;}
+	timer = setInterval("change(true)", 3000);
+}
+
+// 切换图片
+function change(isToRight){
+	images[index].className = "";
+	tabs[index].className = "";
+	if (isToRight) {
+		index ++;
+		if (index > images.length - 1) index = 0;
+	} else {
+		index --;
+		if (index < 0) index = images.length - 1;
+	}
+	images[index].className = "on";
+	tabs[index].className = "on";
+}
