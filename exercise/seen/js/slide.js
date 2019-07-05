2c4bd0eb91f29f5893dab3b651fb7bea39bc2db7
oral0717 <807910824@qq.com>
2019-07-05 23:57:01 +0800

    update

--- exercise/seen/js/slide.js
+++ exercise/seen/js/slide.js
@@ -1,52 +1,46 @@
-var banner = document.querySelector(".banner");
-var images = document.querySelectorAll(".img li");
-var tabsParent = document.querySelector(".btn-list");
-var tabs = document.querySelectorAll(".btn-list li")
-var index = 0;
-
-// 定时器
-var timer = null;
-
 window.onload = function(){
-	timer = setInterval("change(true)", 3000);
+  var images = document.querySelectorAll(".img li");
+  var tabs = document.querySelectorAll(".btn-list li")
+  var index = 0;
+  var timer = null;
+  timer = setInterval(function(){
+    change(true);
+  }, 3000);
+
+  for(var i=0;i<tabs.length;i++){
+    tabs[i].onmouseover = function(e){
+      clearInterval(timer);
+      var curTab = event.target;
+      index = parseInt(curTab.dataset.index);
+      if(index >= 0){
+        for(var i=0;i<images.length;i++){
+          images[i].className = "";
+          tabs[i].className = "";
+        }
+        images[index].className = "on";
+        tabs[index].className = "on";
+      }
+    }
+    tabs[i].onmouseout = function(){
+      timer = setInterval(function(){
+        change(true);
+      }, 3000);
+    }
+  }
+
+  // 切换图片
+  var change = function(isToRight){
+    images[index].className = "";
+    tabs[index].className = "";
+    if (isToRight) {
+      index ++;
+      if (index > images.length - 1) index = 0;
+    } else {
+      index --;
+      if (index < 0) index = images.length - 1;
+    }
+    images[index].className = "on";
+    tabs[index].className = "on";
+  }
+
 };
-
-
-tabsParent.onmouseover = function(event){
-	var curTab = event.target;
-	var curTabTag = curTab.tagName.toLowerCase();
-	if (curTabTag !== "li") {return false;}
-	clearInterval(timer);
-	index = parseInt(curTab.dataset.index);
-	if(index >= 0){
-		for(var i=0;i<images.length;i++){
-			images[i].className = "";
-			tabs[i].className = "";
-		}
-		images[index].className = "on";
-		tabs[index].className = "on";
-	}
-
-}
-
-tabsParent.onmouseout = function(event) {
-	var curTab = event.target;
-	var curTabTag = curTab.tagName.toLowerCase();
-	if (curTabTag !== "li") {return false;}
-	timer = setInterval("change(true)", 3000);
-}
-
-// 切换图片
-function change(isToRight){
-	images[index].className = "";
-	tabs[index].className = "";
-	if (isToRight) {
-		index ++;
-		if (index > images.length - 1) index = 0;
-	} else {
-		index --;
-		if (index < 0) index = images.length - 1;
-	}
-	images[index].className = "on";
-  tabs[index].className = "on";
-}
