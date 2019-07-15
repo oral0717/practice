9cf2e4a29d2e409af96397d81e1324e9bca7b153
oral0717 <807910824@qq.com>
2019-07-15 10:45:26 +0800

    seen-pad rename  update

--- exercise/seen-pad/js/slide.js
+++ exercise/seen-pad/js/slide.js
@@ -1,6 +1,6 @@
 window.onload = function(){
-  var images = document.querySelectorAll(".seen-imp-img li");
-  var tabs = document.querySelectorAll(".seen-imp-btn-list li")
+  var images = document.querySelectorAll(".seenpad-imp-img li");
+  var tabs = document.querySelectorAll(".seenpad-imp-btn-list li")
   var index = 0;
   var timer = null;
   timer = setInterval(function(){
@@ -17,8 +17,8 @@ window.onload = function(){
           images[i].className = "";
           tabs[i].className = "";
         }
-        images[index].className = "seen-imp-on";
-        tabs[index].className = "seen-imp-on";
+        images[index].className = "seenpad-imp-on";
+        tabs[index].className = "seenpad-imp-on";
       }
     }
     tabs[i].onmouseout = function(){
@@ -27,7 +27,7 @@ window.onload = function(){
       }, 3000);
     }
   }
-  $("#seenImpBanner").touchMoveDirection({
+  $("#seenPadImpBanner").touchMoveDirection({
     　　 onLeft: function () {
           // console.log("left");
           clearInterval(timer);
@@ -56,8 +56,8 @@ window.onload = function(){
       index --;
       if (index < 0) index = images.length - 1;
     }
-    images[index].className = "seen-imp-on";
-    tabs[index].className = "seen-imp-on";
+    images[index].className = "seenpad-imp-on";
+    tabs[index].className = "seenpad-imp-on";
   }
 
 };
